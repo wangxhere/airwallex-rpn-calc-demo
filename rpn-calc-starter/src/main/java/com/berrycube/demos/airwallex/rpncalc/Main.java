a4e2cd12de6fdeab02814cbb668c33b7ff8c6974
Wang Xian <wangxhere@gmail.com>
2020-10-31 09:58:36 +0800

    main

--- rpn-calc-starter/src/main/java/com/berrycube/demos/airwallex/rpncalc/Main.java
+++ rpn-calc-starter/src/main/java/com/berrycube/demos/airwallex/rpncalc/Main.java
@@ -1,13 +1,18 @@
 package com.berrycube.demos.airwallex.rpncalc;
 
+import com.berrycube.demos.airwallex.rpncalc.controller.CliController;
 import com.berrycube.demos.airwallex.rpncalc.controller.CliControllerImpl;
 import com.berrycube.demos.airwallex.rpncalc.domain.RpnCalculator;
 import com.berrycube.demos.airwallex.rpncalc.domain.RpnCalculatorImpl;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
+
+    private static final String PROMPT = "Input empty line to terminate.";
+    private static final String EXIT_MSG = "Thank you for using.";
 
     public static void main(String[] args) {
 	    // design consideration: given this is a CLI without much scalable concerns, stack size is not explicitly controlled
@@ -15,6 +20,20 @@ public class Main {
         Stack<BigDecimal> stack = new Stack<>();
         RpnCalculator calculator = new RpnCalculatorImpl(history, stack);
 
-        CliControllerImpl uut = new CliControllerImpl(calculator);
+        CliController cli = new CliControllerImpl(calculator);
+
+        System.out.println(PROMPT);
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        while (! line.isEmpty()) {
+            System.out.println(cli.processInput(line));
+            line = scanner.nextLine();
+        };
+
+        System.out.println(EXIT_MSG);
+
+        scanner.close();
     }
 }
